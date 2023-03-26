06b31c9b42673967489bcbf18eb22086c48fe4e7
swam16 <swatirajpoot16@gmail.com>
2023-03-26 12:34:52 +0530

    Initial commit

---
+++ public/javascripts/project.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $.getJSON('/books/fetch_all_subjects', function(data){
+        data.map((item)=>{
+          $('#subjects').append($('<option>').text(item.subjectname).val(item.subjectid))
+        })
+
+    $('#subjects').change(function(){
+      $.getJSON('/books/fetch_all_titles',{subjectid:$('#subjects').val()},function(data){
+
+        $('#title').empty()
+        $('#title').append($('<option>').text('-Select Title-'))
+
+        data.map((item)=>{
+
+          $('#title').append($('<option>').text(item.titlename).val(item.titleid))
+        })
+      })
+    })
+
+    })
+})
